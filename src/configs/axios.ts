035b0863c36d9709dc714b1d2e025c60d7e5b531
Leily Hasanzadeh <hasanzadeh.farokh@gmail.com>
2024-05-07 07:45:44 +0430

    remove toastify

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,6 +1,5 @@
 //libraries
 import axios, { AxiosError } from "axios";
-import { toast } from "react-toastify";
 
 // axios.defaults.baseURL = "http://test.ir";
 
@@ -12,27 +11,25 @@ axios.interceptors.response.use(
   (res) => res,
   (error: AxiosError) => {
     if (error.code === "ERR_NETWORK") {
-      toast.error("خطای داخلی رخ داده است.");
+      console.error("خطای داخلی رخ داده است.");
       return;
     }
 
     const { status } = error.response!;
     switch (status) {
       case 400:
-        toast.error("400");
+        console.error("400");
         break;
 
       case 401:
-        toast.error(
-          "توکن شما منقضی شده است، لطفا دوباره وارد حساب کاربری خود شوید."
-        );
+        console.error("401");
         break;
 
       case 500:
-        toast.error("خطای سیستمی رخ داده است.");
+        console.error("500");
         break;
       default:
-        if (status !== 404) toast.error("خطای داخلی رخ داده است.");
+        if (status !== 404) console.error("404");
     }
     return Promise.reject(error);
   }
